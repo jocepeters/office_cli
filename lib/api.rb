c5209eb4475e0d8ebcaa301dc63f570753bdcfac
Jocelyn Peters <jnutter1990@gmail.com>
2020-12-12 14:31:55 -0500

    API WORKING

--- lib/api.rb
+++ lib/api.rb
@@ -1,32 +1,24 @@
 require_relative "../config/environment"
 
-class EpisodeIndex::API
-    attr_reader :uri
-
-
-    def initialize
+module EpisodeIndex
+   
+  
+    class API
+      def initialize
         @url = "https://www.officeapi.dev/api/episodes?limit=400"
-        @uri = URI.parse(@url)
-        end
-
-
-    def get_episode_data
+      end
+  
+      def titles
         uri = URI.parse(@url)
-        response = Net::HTTP.get_response(uri)
-        data = JSON.parse(response.body)
-        data["data"].each do |episode|
-            get_episode_title(episode["title"])
-        end
-    end
-
-    def get_episode_title(title)
-        uri = URI.parse(title)
-        response = Net::HTTP.get_response(title)
+        response = Net::HTTP.get(uri)
+        
         data = JSON.parse(response)
+        data["data"].map do |episode|
+            episode["title"]
+            binding.pry
+        end
+      end
     end
+  end
 
-    EpisodeIndex::API.new.get_episode_data
-
-end
-
- 
+  EpisodeIndex::API.new.titles
